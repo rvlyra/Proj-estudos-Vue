817a2c5fa16de168f00055829713273bda9bb191
Rubens Lyra <rubensvlyra@gmail.com>
2021-10-30 18:42:53 -0300

    Iniciando estudos

---
+++ aula_03/app.js
@@ -0,0 +1,13 @@
+
+const app = {
+    data () {
+        return {
+            name: 'Rubens',
+            lastName: 'Lyra'
+        }
+    }
+};
+
+Vue.createApp(app).mount("#app");
+
+// Vue.createApp({}).mount("#app");
