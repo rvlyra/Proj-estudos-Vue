aa23dcbedbd4dcde596e5065c32cc4c0acbd7062
Rubens Lyra <rubensvlyra@gmail.com>
2021-11-01 23:45:21 -0300

    aula 05

---
+++ aula_05/app.js
@@ -0,0 +1,25 @@
+
+const app = {
+    data () {
+        return {
+            name: 'Rubens',
+            lastName: 'Lyra',
+            product: {
+                title: 'Bolo de Morango',
+                description: 'Sem glúten, açúcar natural, morangos fresquinhos e ideal para acompanhar o nosso Latte Macchiato',
+                image: './assets/img/bolo.png',
+                stars: 3,
+            },
+            product2: {
+                title: 'Pão caseiro',
+                description: 'Sem glúten, sódio (491 mg) e baixíssimo valor calórico',
+                image: './assets/img/pao.png',
+                stars: 0,
+            },
+        }
+    }
+};
+
+Vue.createApp(app).mount("#app");
+
+// Vue.createApp({}).mount("#app");
